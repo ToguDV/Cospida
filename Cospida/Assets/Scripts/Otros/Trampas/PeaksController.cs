a5d5d0b9033f62a75957613e2659b20ae7ee6af0
TOGU <timeofgetup@gmail.com>
2021-07-23 22:10:23 -0500

    trampas agregadas

---
+++ Cospida/Assets/Scripts/Otros/Trampas/PeaksController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeaksController : MonoBehaviour
+{
+    public Animator animator;
+    public bool manual;
+    public float timeDelay;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Playervul") && !manual)
+        {
+            Invoke("ManualActivation", timeDelay);
+        }
+    }
+
+    public void ManualActivation()
+    {
+        animator.SetBool("isActive", true);
+    }
+
+
+    public void ManualDesactivation()
+    {
+        animator.SetBool("isActive", false);
+    }
+}
